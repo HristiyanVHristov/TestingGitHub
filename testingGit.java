85662ec501701e692f5b32e44c1e18849f5f0885
Hristiyan Hristov <hristiyanvh99@gmail.com>
2023-10-10 22:07:19 +0300

    Using spaces

--- testingGit.java
+++ testingGit.java
@@ -1,3 +1,6 @@
-int num1 = 3;
-int num2 = 2;
-System.out.println(num1 + num2);
+public static void main(String[]args){
+
+        int num1 = 3;
+        int num2 = 2;
+        System.out.println(num1 + num2);
+        }
